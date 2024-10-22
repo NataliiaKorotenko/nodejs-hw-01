d98f8336deefbb2847d62a7b621d3311b82f4d6a
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2024-10-22 13:10:01 +0200

    fini

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
+
+import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = constants => writeFile(PATH_DB, JSON.stringify(constants, null, 2));
